32369f69dbb2c95db41a212f90338f1f62983b86
MiladSedhom <MiladSedhom@gmail.com>
2024-07-26 16:36:12 +0300

    refactor: fetch students

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -44,7 +44,8 @@ class StudentView extends StatefulWidget {
 
 class _StudentViewState extends State<StudentView> {
   List<Student> students = [];
-  bool isLoading = false;
+  bool isLoading = true;
+  String error = '';
 
   final limit = 5;
   var page = 0;
@@ -54,9 +55,7 @@ class _StudentViewState extends State<StudentView> {
   @override
   void initState() {
     super.initState();
-    isLoading = true;
     fetchStudent();
-    isLoading = false;
   }
 
   @override
@@ -64,21 +63,34 @@ class _StudentViewState extends State<StudentView> {
     return Column(
       children: [
         Expanded(
-          child: isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : students.isNotEmpty
-                  ? ListView.builder(
-                      itemCount: students.length,
-                      itemBuilder: (context, int index) {
-                        return ListTile(
-                          title: Text(students[index].firstName),
-                          subtitle: Text(students[index].lastName),
-                        );
-                      },
-                    )
-                  : const Center(
-                      child: Text("No More Students"),
-                    ),
+          child: Builder(
+            builder: (context) {
+              if (isLoading == true) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (error.isNotEmpty) {
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(error),
+                  ),
+                );
+              } else if (students.isNotEmpty) {
+                return ListView.builder(
+                  itemCount: students.length,
+                  itemBuilder: (context, int index) {
+                    return ListTile(
+                      title: Text(students[index].firstName),
+                      subtitle: Text(students[index].lastName),
+                    );
+                  },
+                );
+              } else {
+                return const Center(
+                  child: Text("No More Students"),
+                );
+              }
+            },
+          ),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -123,25 +135,13 @@ class _StudentViewState extends State<StudentView> {
             TextButton(
                 onPressed: () async {
                   page = page > 0 ? page - 1 : 0;
-                  setState(() {
-                    isLoading = true;
-                  });
                   fetchStudent();
-                  setState(() {
-                    isLoading = false;
-                  });
                 },
                 child: const Text("Previous")),
             TextButton(
                 onPressed: () async {
                   page = page + 1;
-                  setState(() {
-                    isLoading = true;
-                  });
                   fetchStudent();
-                  setState(() {
-                    isLoading = false;
-                  });
                 },
                 child: const Text("Next")),
           ],
@@ -152,24 +152,40 @@ class _StudentViewState extends State<StudentView> {
 
   Future<void> fetchStudent() async {
     const String url = 'http://localhost:3000/students';
-    final res = await http.get(Uri.parse('$url?limit=$limit&page=$page&sortBy=$sortBy&sortType=$sortType'));
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      final res = await http.get(Uri.parse('$url?limit=$limit&page=$page&sortBy=$sortBy&sortType=$sortType'));
 
-    if (res.statusCode == 200) {
-      List<dynamic> data = jsonDecode(res.body);
+      if (res.statusCode == 200) {
+        List<dynamic> data = jsonDecode(res.body);
+        setState(() {
+          students = data.map((item) => Student.fromJson(item)).toList();
+        });
+      } else {
+        setState(() {
+          error = 'Error getting students, server responded with code: ${res.statusCode}';
+        });
+      }
+    } catch (e) {
       setState(() {
-        students = data.map((item) => Student.fromJson(item)).toList();
+        error = e.toString();
       });
-    } else {
-      throw Exception("Failed to load Students, Status code: ${res.statusCode}");
     }
+    setState(() {
+      isLoading = false;
+    });
   }
 }
 
 class Student {
+  final String id;
   final String firstName;
   final String lastName;
 
   Student({
+    required this.id,
     required this.firstName,
     required this.lastName,
   });
@@ -177,14 +193,16 @@ class Student {
   factory Student.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
+        '_id': String id,
         'firstName': String firstName,
         'lastName': String lastName,
       } =>
         Student(
+          id: id,
           firstName: firstName,
           lastName: lastName,
         ),
-      _ => throw const FormatException('Failed to load album.'),
+      _ => throw const FormatException('Failed to load student.'),
     };
   }
 }
