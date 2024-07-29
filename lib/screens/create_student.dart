5f48f85412f9227614d0ba27071f6cc4d260bb51
MiladSedhom <MiladSedhom@gmail.com>
2024-07-29 16:48:37 +0300

    add addresses fields

--- lib/screens/create_student.dart
+++ lib/screens/create_student.dart
@@ -34,7 +34,20 @@ class _CreateStudentFormState extends State<CreateStudentForm> {
   final TextEditingController lastNameController = TextEditingController();
 
   List<Skill> skills = [];
+  List<Address> addresses = [Address(country: '', city: '', street1: '', street2: '')];
   List<Skill> selectedSkills = [];
+
+  void addAddress() {
+    setState(() {
+      addresses.add(Address(country: '', city: '', street1: '', street2: ''));
+    });
+  }
+
+  void removeAddressAt(int index) {
+    setState(() {
+      addresses.removeAt(index);
+    });
+  }
 
   @override
   void initState() {
@@ -46,13 +59,21 @@ class _CreateStudentFormState extends State<CreateStudentForm> {
     const String url = 'http://localhost:3000/students';
 
     var skills = selectedSkills.map((skill) => {'_id': skill.id}).toList();
+    var addressesMap = addresses
+        .map((address) =>
+            {'country': address.country, 'city': address.city, 'street1': address.street1, 'street2': address.street2})
+        .toList();
 
     final res = await http.post(Uri.parse(url),
         headers: {
           'Content-Type': 'application/json',
         },
-        body:
-            jsonEncode({'firstName': firstNameController.text, 'lastName': lastNameController.text, 'skills': skills}));
+        body: jsonEncode({
+          'firstName': firstNameController.text,
+          'lastName': lastNameController.text,
+          'skills': skills,
+          'addresses': addressesMap
+        }));
 
     return res;
   }
@@ -74,95 +95,223 @@ class _CreateStudentFormState extends State<CreateStudentForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        key: formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              TextFormField(
-                controller: firstNameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'First Name',
-                ),
-                autofocus: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your first name.';
-                  }
-                  if (value.length < 2 || value.length > 20) {
-                    return 'Names must be between 2 and 20 characters.';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                controller: lastNameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Last Name',
-                ),
-                autofocus: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your first name.';
-                  }
-                  if (value.length < 2 || value.length > 20) {
-                    return 'Names must be between 2 and 20 characters.';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              MultiSelectDialogField(
-                buttonText: const Text("Choose Student's skills..."),
-                decoration: BoxDecoration(
-                  color: Colors.blue.withOpacity(0.1),
-                  borderRadius: const BorderRadius.all(Radius.circular(4)),
-                  border: Border.all(
-                    width: 1,
+    return SingleChildScrollView(
+      child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: firstNameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'First Name',
                   ),
+                  autofocus: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your first name.';
+                    }
+                    if (value.length < 2 || value.length > 20) {
+                      return 'Names must be between 2 and 20 characters.';
+                    }
+                    return null;
+                  },
                 ),
-                searchHint: "Choose Student's skills...",
-                title: const Text("Choose Student's skills"),
-                searchable: true,
-                items: skills.map((skill) => MultiSelectItem(skill, skill.name)).toList(),
-                onConfirm: (values) {
-                  selectedSkills = values;
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: lastNameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Last Name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your first name.';
+                    }
+                    if (value.length < 2 || value.length > 20) {
+                      return 'Names must be between 2 and 20 characters.';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                MultiSelectDialogField(
+                  buttonText: const Text("Choose Student's skills..."),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.withOpacity(0.1),
+                    borderRadius: const BorderRadius.all(Radius.circular(4)),
+                    border: Border.all(
+                      width: 1,
+                    ),
+                  ),
+                  searchHint: "Choose Student's skills...",
+                  title: const Text("Choose Student's skills"),
+                  searchable: true,
+                  items: skills.map((skill) => MultiSelectItem(skill, skill.name)).toList(),
+                  onConfirm: (values) {
+                    selectedSkills = values;
+                  },
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: addresses.length,
+                  itemBuilder: (context, index) {
+                    return AddressFields(index, addresses, removeAddressAt);
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(onPressed: addAddress, child: const Text('Add Another Address')),
+                ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      postStudent().then((res) {
+                        if (res.statusCode == 200) {
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                            content: Text("Student created successfully!"),
+                            duration: Duration(seconds: 1),
+                          ));
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                            content: Text("Failed to create Student!"),
+                            duration: Duration(seconds: 1),
+                          ));
+                        }
+                      });
+                      Future.delayed(const Duration(seconds: 1), () {
+                        Navigator.of(context).pop();
+                      });
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class AddressFields extends StatelessWidget {
+  const AddressFields(this.index, this.addresses, this.removeAddressAt, {super.key});
+
+  final int index;
+  final List<Address> addresses;
+  final void Function(int) removeAddressAt;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                onChanged: (value) {
+                  addresses[index].country = value;
                 },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    postStudent().then((res) {
-                      if (res.statusCode == 200) {
-                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          content: Text("Student created successfully!"),
-                          duration: Duration(seconds: 1),
-                        ));
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          content: Text("Failed to create Student!"),
-                          duration: Duration(seconds: 1),
-                        ));
-                      }
-                    });
-                    Future.delayed(const Duration(seconds: 1), () {
-                      Navigator.of(context).pop();
-                    });
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Country',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your country.';
                   }
+                  if (value.length < 2 || value.length > 20) {
+                    return 'Country name must be between 2 and 20 characters.';
+                  }
+                  return null;
                 },
-                child: const Text('Submit'),
               ),
-            ],
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: TextFormField(
+                onChanged: (value) {
+                  addresses[index].city = value;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'City',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your city.';
+                  }
+                  if (value.length < 2 || value.length > 20) {
+                    return 'City name must be between 2 and 20 characters.';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        TextFormField(
+          onChanged: (value) {
+            addresses[index].street1 = value;
+          },
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Steet1',
           ),
-        ));
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter your street.';
+            }
+            if (value.length < 2 || value.length > 20) {
+              return 'Street name must be between 2 and 20 characters.';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                onChanged: (value) {
+                  addresses[index].street2 = value;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Steet2',
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            IconButton.filled(
+                onPressed: () {
+                  removeAddressAt(index);
+                },
+                icon: const Icon(Icons.delete)),
+          ],
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+      ],
+    );
   }
 }
 
@@ -187,5 +336,24 @@ class Skill {
         ),
       _ => throw const FormatException('Failed to load student.'),
     };
+  }
+}
+
+class Address {
+  String country;
+  String city;
+  String street1;
+  String street2;
+
+  Address({
+    required this.country,
+    required this.city,
+    required this.street1,
+    required this.street2,
+  });
+
+  @override
+  String toString() {
+    return '{ $country, $city, $street1, $street2 }';
   }
 }
